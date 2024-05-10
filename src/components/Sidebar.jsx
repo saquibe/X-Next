e107c59ef9405a10d7405763e6d5dbbb64702ad4
mohammad saquib <saquibe143@gmail.com>
2024-05-10 19:39:52 +0530

    add mini profile to sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,13 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
-import { HiHome } from "react-icons/hi";
+import { HiDotsHorizontal, HiHome } from "react-icons/hi";
 
 export default function Sidebar() {
   const {data: session } = useSession();
   return (
-    <div className="flex flex-col gap-4 p-3">
+    <div className="flex flex-col p-3 justify-between h-screen">
+      <div className="flex flex-col gap-4 p-3">
       <Link href='/'>
       <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
       </Link>
@@ -22,14 +23,24 @@ export default function Sidebar() {
           Sign Out
         </button>
 
-      ) : (
-      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md font-semibold hidden lg:inline"
-      onClick={() => signIn()}
-      >
+) : (
+  <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md font-semibold hidden lg:inline"
+  onClick={() => signIn()}
+  >
         Sign In
       </button>
     )}
-
+    </div>
+      {session && (
+        <div className="text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200">
+            <img src={session.user.image} alt="user-img" className="h-10 w-10 rounded-full lg:mr-2" />
+            <div className="hidden lg:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <HiDotsHorizontal className="h-5 lg:ml-8 hidden lg:inline" />
+        </div>
+    )}
     </div>
   );
 }
