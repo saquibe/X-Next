3d2d94404f53b40df00d97eb2a3ec475bfd6b89d
mohammad saquib <saquibe143@gmail.com>
2024-05-10 10:40:24 +0530

    add next-auth and modify the sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi";
 
 export default function Sidebar() {
+  const {data: session } = useSession();
   return (
     <div className="flex flex-col gap-4 p-3">
       <Link href='/'>
@@ -10,11 +13,23 @@ export default function Sidebar() {
       </Link>
       <Link href='/' className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit">
         <HiHome className="h-7 w-7" />
-        <span className="font-bold hidden xl:inline">Home</span>
+        <span className="font-bold hidden lg:inline">Home</span>
       </Link>
-      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md font-semibold hidden xl:inline">
+      {session ? (
+        <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md font-semibold hidden lg:inline"
+        onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+
+      ) : (
+      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md font-semibold hidden lg:inline"
+      onClick={() => signIn()}
+      >
         Sign In
       </button>
+    )}
+
     </div>
   );
 }
