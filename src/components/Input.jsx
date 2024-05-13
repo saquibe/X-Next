6385320466acbf9086136df8ae2bb026236637c1
mohammad saquib <saquibe143@gmail.com>
2024-05-13 10:56:38 +0530

    add upload post functionality to the firestore

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,13 +5,17 @@ import { useEffect, useRef, useState } from "react";
 import { HiOutlinePhotograph } from "react-icons/hi";
 import {app} from '../firebase';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { addDoc, collection, getFireStore, serverTimestamp } from 'firebase/firestore';
 
 export default function Input() {
   const { data: session } = useSession();
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [text, setText] = useState('');
+  const [postLoading, setPostLoading] = useState(false);
   const imagePicRef = useRef(null);
+  const db = getFireStore(app);
   const addImageToPost = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -53,6 +57,23 @@ export default function Input() {
     );
   };
 
+    const handleSubmit = async () => {
+      setPostLoading(true);
+      const docRef = await addDoc(collection(db, 'posts'), {
+        uid: session.user.uid,
+        name: session.user.name,
+        username: session.user.username,
+        text,
+        profileImg: session.user.image,
+        timestamp: serverTimestamp(),
+        image: imageFileUrl,
+      });
+      setPostLoading(false);
+      setText('');
+      setImageFileUrl(null);
+      setSelectedFile(null);
+    };
+
   if (!session) return null;
   return (
     <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
@@ -63,12 +84,15 @@ export default function Input() {
       <div className="w-full divide-y divide-gray-200">
         <textarea 
           className="w-full border-none outline-none tracking-wide min-h-[50] text-gray-700" placeholder="What's happening" 
-          rows="2" ></textarea>
+          rows="2"
+          value={text}
+          onChange={(e) => setText(e.target.value)} 
+        ></textarea>
           {selectedFile && (
             <img 
               src={imageFileUrl} 
               alt="image"
-              className="w-full max-h-[250px] object-cover cursor-pointer" 
+              className={`w-full max-h-[250px] object-cover cursor-pointer ${imageFileUploading ? 'animate-pulse' : ''}`} 
             />
           )}
         <div className="flex items-center justify-between pt-2.5">
@@ -80,7 +104,13 @@ export default function Input() {
           ref={imagePicRef} accept='image/*' 
           onChange={addImageToPost} 
           hidden/>
-          <button disabled className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Post</button>
+          <button 
+          disabled={text.trim() === '' || postLoading || imageFileUploading} 
+          className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+          onClick={handleSubmit}
+          >
+            Post
+          </button>
         </div>
       </div>
     </div>
