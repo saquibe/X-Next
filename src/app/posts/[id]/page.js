20bd03cd4dc7c2a28cd93eb59a2fe451af862488
mohammad saquib <saquibe143@gmail.com>
2024-05-22 08:30:35 +0530

    Update postPage comment to fetch data from firebase and display post details

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import Post from "@/components/Post";
+import { app } from "@/firebase"
+import { doc, getDoc, getFirestore } from "firebase/firestore"
+import Link from "next/link";
+import { HiArrowLeft } from "react-icons/hi";
 
-export default function page() {
+export default async function PostPage({ params }) {
+  const db = getFirestore(app);
+  let data = {};
+  const querySnapshot = await getDoc(doc(db, 'posts', params.id));
+  data = {...querySnapshot.data(), id: querySnapshot.id};
+
   return (
-    <div>page</div>
+    <div className="max-w-lg mx-auto border-r border-l min-h-screen">
+      <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <Link href={'/'} className="hover:bg-gray-100 rounded-full p-2">
+          <HiArrowLeft className="h-5 w-5"/>
+        </Link>
+        <h2 className="sm:text-lg">Back</h2>
+      </div>
+      <Post post={data} id={data.id}/>
+    </div>
   )
 }
