9af5859b3bfb68ea5e6ae526e927aa65fa81525f
mohammad saquib <saquibe143@gmail.com>
2024-05-22 15:56:50 +0530

    Add like functionality to Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,42 @@
 "use client";
 
-import { HiDotsHorizontal } from "react-icons/hi";
+import { app } from "@/firebase";
+import { collection, deleteDoc, doc, getFirestore, onSnapshot, serverTimestamp, setDoc } from "firebase/firestore";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { HiDotsHorizontal, HiHeart, HiOutlineHeart } from "react-icons/hi";
 
-export default function Comment({ comment, id }) {
+export default function Comment({ comment, id, commentId, originalPostId }) {
+    const [isLiked, setIsLiked] = useState(false);
+    const [likes, setLikes] = useState([]);
+    const db = getFirestore(app);
+
+    const likePost = async () => {
+        if (session) {
+          if (isLiked) {
+            await deleteDoc(doc(db, 'posts', originalPostId, 'comments', commentId, 'likes', session?.user.uid));
+          }else{
+            await setDoc(doc(db, 'posts', originalPostId, 'comments', commentId, 'likes', session.user.uid), {
+              username: session.user.username,
+              timestamp: serverTimestamp(),
+            });          
+          }
+        }else{
+          signIn();
+        }
+      };
+
+      useEffect(() => {
+        onSnapshot(collection(db, 'posts', originalPostId, 'comments', commentId, 'likes'), (snapshot) => {
+          setLikes(snapshot.docs);
+        })
+      }, [db]);
+  
+      useEffect(() => {
+        setIsLiked(
+          likes.findIndex((like) => like.id === session?.user?.uid) != -1
+        );
+      }, [likes]);
   return (
     <div className="flex p-3 border-b border-gray-200 hover:bg-gray-50 pl-10">
       <img src={comment?.userImg} alt="user-img" className="h-9 w-9 rounded-full mr-4"/>
@@ -15,6 +49,18 @@ export default function Comment({ comment, id }) {
           <HiDotsHorizontal className="text-sm"/>
         </div>
           <p className="text-gray-800 text-xs my-3">{comment?.comment}</p>
+          <div className="felx items-center">
+          {isLiked ? (
+        <HiHeart
+          onClick={likePost} 
+          className="h-8 w-8  cursor-pointer rounded-full transition text-red-500 duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"/>
+      ) : (
+        <HiOutlineHeart
+          onClick={likePost} 
+          className="h-8 w-8  cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"/>        
+      )}
+      {likes.length > 0 && (<span className={`text-xs ${isLiked && 'text-red-600'}`}>{likes.length}</span>)}
+          </div>
       </div>
     </div>
   )
